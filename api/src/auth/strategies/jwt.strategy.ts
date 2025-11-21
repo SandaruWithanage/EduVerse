33f08575b99795d4bb8d372bb41be7552525d4d7
SandaruWithanage <160226615+SandaruWithanage@users.noreply.github.com>
2025-11-21 20:09:48 +0530

    fix: remove extraneous character in JwtStrategy class definition

--- api/src/auth/strategies/jwt.strategy.ts
+++ api/src/auth/strategies/jwt.strategy.ts
@@ -4,7 +4,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class JwtStrategy extends PassportStrategy(Strategy) {g
   constructor() {
     super({
       // Where to read the token from:
